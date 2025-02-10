a8688d0a139a3c8fb4fcac358b034f9c07f84433
abdlmnf17 <11210195@bsi.ac.id>
2025-02-10 09:06:44 +0700

    modify beberapayg bugs

--- src/components/js/header.ts
+++ src/components/js/header.ts
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (!header || !hamburger || !menu || !overlay) return;
 
-  // Fungsi untuk toggle menu
+ 
   const toggleMenu = () => {
     menu.classList.toggle("active");
     hamburger.classList.toggle("active");
@@ -15,15 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.style.overflow = menu.classList.contains("active") ? "hidden" : "";
   };
 
-  // Fungsi untuk update header saat scroll
+
   const updateHeader = () => {
     if (!heroSection) return;
-
-    // Menentukan posisi scroll
     const scrollY = window.scrollY;
 
-    // Ganti header saat scroll Y melebihi tinggi heroSection
-    if (scrollY > heroSection.offsetHeight / 5) { // Ganti batas ini sesuai kebutuhan
+    if (scrollY > heroSection.offsetHeight / 5) { 
       header.classList.remove("transparent");
       header.classList.add("white");
     } else {
@@ -32,9 +29,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  // Event listeners
   hamburger.addEventListener("click", toggleMenu);
   overlay.addEventListener("click", toggleMenu);
   window.addEventListener("scroll", updateHeader);
-  updateHeader(); // Set kondisi awal header
+  updateHeader(); 
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Abdul Manap';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'ABDULMANAP.COM - Situs personal dari Abdul Manap, seorang Blogger, Software Engineer dan Accounting. Weblog ini membahas segala sesuatu tentang Teknologi, Belajar Membuat Website, WordPress dan Bahasa Pemrogramman';
