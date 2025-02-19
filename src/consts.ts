cd8e36dae8d1aea305b0bbf167b61b3b7e66cc3c
abdlmnf17 <11210195@bsi.ac.id>
2025-02-19 23:59:53 +0700

    fix meta desc

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Abdul Manap - My Personal Website';
-export const SITE_DESCRIPTION = 'ABDULMANAP.COM - A personal site that discusses everything about Technology, Accounting, Gaming, Learning Web Development, WordPress, and Programming Languages';
+export const SITE_DESCRIPTION = 'ABDULMANAP.COM - I am a Software Developer with Full Stack Experience. I design and develop fast, functional, and SEO-friendly web apps, focusing on architecture and performance.';
 
 export const SITE_TITLE_BLOG = 'Abdul Manap Blog | All About Tech, Accounting and Programming';
 export const SITE_DESCRIPTION_BLOG = 'ABDULMANAP.COM - A blog that discusses everything about Technology, Accounting, Gaming, Learning Web Development, WordPress, and Programming Languages';
